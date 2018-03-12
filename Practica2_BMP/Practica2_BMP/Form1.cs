1c4f90bc698116aee9241099eb871f9fd786fea1
Ricky1904 <36781770+Ricky1904@users.noreply.github.com>
2018-03-11 22:23:16 -0600

    Add files via upload

---
+++ Practica2_BMP/Practica2_BMP/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Practica2_BMP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            FileStream archivo = new FileStream(openFileDialog1.FileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(archivo);
+            char c1 = br.ReadChar();
+            char c2 = br.ReadChar();
+
+            if(c1=='B' && c2 == 'M')
+            {
+                int tam = br.ReadInt32();
+                br.ReadInt64();
+                br.ReadInt32();
+
+                int ancho = br.ReadInt32();
+                int alto = br.ReadInt32();
+
+                br.ReadInt16();
+                Int16 bitspixel;
+                bitspixel = br.ReadInt16();
+
+            }
+            br.Close();
+        }
+    }
+}
