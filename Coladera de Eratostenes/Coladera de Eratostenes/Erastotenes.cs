17733d07759ccf55d5c31dc56255a897ddbfa48f
Ricky1904 <36781770+Ricky1904@users.noreply.github.com>
2018-03-23 23:58:25 -0600

    Add files via upload

---
+++ Coladera de Eratostenes/Coladera de Eratostenes/Erastotenes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coladera_de_Eratostenes
+{
+    class Eratostenes  //clase principal de eratostenes
+    {
+        static string a;
+
+
+
+        static void iniciar()
+
+        {
+
+            int n = 1001; 
+
+            bool[] marcados = new bool[n + 1];
+            StringBuilder builder = new StringBuilder();
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (!marcados[i]) //desigancion numeros no primos
+                    for (int j = i * 2; j <= n; j += i)
+                        marcados[j] = true; 
+            }
+
+            for (int i = 2; i < marcados.Length; i++) 
+                if (marcados[i] == false) 
+                    builder.Append(i + ", ");
+            builder.Length -= 2;
+            a = builder.ToString();
+        }
+        public void star()
+        {
+            iniciar();
+        }
+        public override string ToString()
+        {
+            return a;
+        }
+
+    }
+
+}
---
+++ Coladera de Eratostenes/Coladera de Eratostenes/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coladera_de_Eratostenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Eratostenes eratostenes1 = new Eratostenes();
+            textBox1.Clear();
+            eratostenes1.star();
+            textBox1.Text = eratostenes1.ToString();
+        }
+    }
+}
